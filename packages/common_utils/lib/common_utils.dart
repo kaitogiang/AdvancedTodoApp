112dc3408af7dead9f7718e6bf082047637ae564
Giang Dai Huy <huygiang001442@gmail.com>
2025-10-06 21:31:39 +0700

    feat: config android native

--- packages/common_utils/lib/common_utils.dart
+++ packages/common_utils/lib/common_utils.dart
@@ -1,5 +1,3 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:flutter_localizations/flutter_localizations.dart';
+
+export './src/core/translations/generated/l10n.dart';
